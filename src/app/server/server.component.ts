ffa7229445a65de598fd497b6984842a503ae12a
ToanPhan <toan.phan@yuriqa.com>
2019-10-10 10:32:24 +0700

    two way data biding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+    serverId = "123";
+    serverSts = "offline";
+    
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,11 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.scss']
 })
-export class ServersComponent implements OnInit {
+export class ServersComponent implements OnInit{
 
-  constructor() { }
+  isDisable: boolean = false;
+  serverState: string = 'Server is offline';
+  serverName: string;
+
+  constructor() {
+    setTimeout(() => {
+        this.isDisable = true
+    }, 3000);
+  }
 
   ngOnInit() {
   }
+
+  onServerChange(): void {
+    this.serverState = "Server is online now" ;
+  }
+
+  // onFormUpdate(event: Event): void {
+  //   this.serverName = (<HTMLInputElement>event.target).value;
+  // }
 
 }
