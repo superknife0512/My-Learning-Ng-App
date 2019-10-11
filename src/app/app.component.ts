de50fa93ea9b2febb16c858e1e5b6ec2908e0e68
ToanPhan <toan.phan@yuriqa.com>
2019-10-11 08:15:33 +0700

    setup out application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,17 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+ 
 export class AppComponent {
-  isDisplay: boolean = false;
-  clickArray: Array<number> = [];
-  increment = 0;
-
-  constructor(){}
-
-  onDisplayDetail(): void {
-    this.isDisplay = !this.isDisplay;
-    this.increment++;
-    this.clickArray.push(this.increment);
-  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './UI/header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
